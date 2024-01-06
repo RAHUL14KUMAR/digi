38c2cfb85e1ffde62fa5e55a23d9d0f206b947e7
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2024-01-06 18:23:16 +0530

    nav

--- app/components/testimonial/Testimonial.js
+++ app/components/testimonial/Testimonial.js
@@ -12,7 +12,7 @@ function Testimonial() {
                 <div className="flex justify-center items-center">
                     <Image src={Vector} width={200} height={200} alt="cols 1" />
                 </div>
-                <p className="mt-5 text-base sm:text-sm md:text-xl font-semibold text-slate-700">I had the pleasure of experiencing the next generation of card solutions with this incredible product. It's refreshing to see such innovation in the financial industry.</p>
+                <p className="mt-5 text-base sm:text-sm md:text-xl font-semibold text-slate-700">I had the pleasure of experiencing the next generation of card solutions with this incredible product. It s refreshing to see such innovation in the financial industry.</p>
 
                 <div className='flex justify-center items-center'>
                     <Image src={Avatar} width={100} height={100} alt="cols 1"/>
